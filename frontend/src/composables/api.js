267be4bdb5ed6ebe0356950c4d32634258c7e9dc
bolohonov <bolohonov@list.ru>
2026-03-02 14:39:40 +0400

    Initial commit

--- frontend/src/composables/api.js
+++ frontend/src/composables/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/' })
+const api = axios.create({ baseURL: '/chat' })
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token')
